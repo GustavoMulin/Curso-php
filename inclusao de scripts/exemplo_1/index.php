97916a640ac175c460e9a0fcf3c4bc5b6bfbe394
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-03-05 12:13:39 -0300

    First commit

---
+++ inclusao de scripts/exemplo_1/index.php
@@ -0,0 +1,4 @@
+<!-- Usando estrutura / cabecalho podemos usar os scripts que estão dentro de outra (path)-->
+<?php require_once('estrutura/cabecalho.php')?>
+<h1>Página inicial</h1>
+<?php require_once('estrutura/rodape.php')?>
---
+++ inclusao de scripts/exemplo_2/public/index.php
@@ -0,0 +1,4 @@
+<!-- Os dois '..' servem para nos andarmos uma pasta para trás-->
+<?php  require_once('../estrutura/cabecalho.php')?>
+<h1>Página inicial</h1>
+<?php require_once('../estrutura/rodape.php')?>
